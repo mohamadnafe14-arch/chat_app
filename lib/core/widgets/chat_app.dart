a14abebfa9706212acbd29055a77e6f25bb66f46
mohamed <mnafe44@gmail.com>
2026-06-23 11:13:29 +0300

    adding colors

--- lib/core/widgets/chat_app.dart
+++ lib/core/widgets/chat_app.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatApp extends StatelessWidget {
   const ChatApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Chat App'))),
+    return ScreenUtilInit(
+      designSize: Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Chat App',
+        home: Scaffold(body: Center(child: Text('Chat App'))),
+      ),
     );
   }
 }
