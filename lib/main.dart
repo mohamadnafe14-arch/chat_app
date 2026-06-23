f925c50e16b8aa16104f471a1a865f92914a07d9
mohamed <mnafe44@gmail.com>
2026-06-23 12:17:02 +0300

    adding dependecy injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:chat_app/core/utils/dependecy_injection.dart';
 import 'package:chat_app/core/widgets/chat_app.dart';
-import 'package:chat_app/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
+final serviceLocator = GetIt.instance;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dependencyInjection();
   runApp(const ChatApp());
 }
