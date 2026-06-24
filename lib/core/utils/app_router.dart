d823bfba624f9dc639cd751e6edb8d7cc42a1740
mohamed <mnafe44@gmail.com>
2026-06-24 13:09:24 +0300

    adding home view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,14 @@
+import 'package:chat_app/features/home/presentation/views/home_view.dart';
 import 'package:chat_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
   static const String initial = '/';
+  static const String home = '/home';
   static final router = GoRouter(
     routes: [
       GoRoute(path: initial, builder: (context, state) => const SplashView()),
+      GoRoute(path: home, builder: (context, state) => const HomeView()),
     ],
   );
 }
