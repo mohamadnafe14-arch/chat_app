98f19314a9be79810c21bffe66ac170811f7d4e6
mohamed <mnafe44@gmail.com>
2026-06-26 00:59:29 +0300

    adding sign up view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/features/auth/presentation/views/sign_up_view.dart';
 import 'package:chat_app/features/home/presentation/views/home_view.dart';
 import 'package:chat_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
@@ -5,10 +6,12 @@ import 'package:go_router/go_router.dart';
 class AppRouter {
   static const String initial = '/';
   static const String home = '/home';
+  static const String signUp = '/signUp';
   static final router = GoRouter(
     routes: [
       GoRoute(path: initial, builder: (context, state) => const SplashView()),
       GoRoute(path: home, builder: (context, state) => const HomeView()),
+      GoRoute(path: signUp, builder: (context, state) => const SignUpView()),
     ],
   );
 }
