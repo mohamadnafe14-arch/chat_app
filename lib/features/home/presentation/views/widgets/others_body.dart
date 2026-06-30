33a6133b5f78c84c2b10c8b56f0bf90d8b8832f7
mohamed <mnafe44@gmail.com>
2026-06-30 19:45:45 +0300

    adding others body

---
+++ lib/features/home/presentation/views/widgets/others_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OthersBody extends StatelessWidget {
+  const OthersBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
