00c158f38b9fab5d08cdfa8281d02febd40149dc
mohamed <mnafe44@gmail.com>
2026-06-30 19:44:38 +0300

    adding style to bottom navigation bar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,37 +14,52 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(child: Text("Home View")),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: currentIndex,
-        onTap: (index) => setState(() => currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Image(
-              image: AssetImage("assets/home_icon.png"),
-              height: 30,
-              width: 30,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey.withValues(alpha: 0.2),
+          borderRadius: BorderRadius.circular(30.r),
+          border: Border.all(color: Colors.grey.withValues(alpha: 0.2)),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(-5, -5),
+              blurRadius: 5,
             ),
-            label: "other",
-          ),
-          BottomNavigationBarItem(
-            icon: Image(
-              image: AssetImage("assets/chats_icon.png"),
-              height: 30,
-              width: 30,
+            BoxShadow(color: Colors.grey, offset: Offset(5, 5), blurRadius: 5),
+          ],
+        ),
+        child: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() => currentIndex = index),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Image(
+                image: AssetImage("assets/home_icon.png"),
+                height: 30,
+                width: 30,
+              ),
+              label: "other",
             ),
-            label: "chats",
-          ),
-          BottomNavigationBarItem(
-            icon: Image(
-              image: AssetImage("assets/profile_icon.png"),
-              height: 30,
-              width: 30,
+            BottomNavigationBarItem(
+              icon: Image(
+                image: AssetImage("assets/chats_icon.png"),
+                height: 30,
+                width: 30,
+              ),
+              label: "chats",
             ),
-            label: "profile",
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: Image(
+                image: AssetImage("assets/profile_icon.png"),
+                height: 30,
+                width: 30,
+              ),
+              label: "profile",
+            ),
+          ],
+        ),
       ),
     );
   }
